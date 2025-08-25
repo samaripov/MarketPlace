9912182769709ebd1f7563fb0333af5983eb8790
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-08-25 16:28:47 -0400

    Creating users completed.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,22 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -10,4 +10,32 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "h1", "Users"
   end
+
+  test "should create user with valid attributes" do
+    assert_difference("User.count") do
+      post users_path, params: {
+        user: {
+          username: "newuser",
+          email: "newuser@example.com",
+          password: "password123",
+          password_confirmation: "password123"
+        }
+      }
+    end
+    assert_redirected_to user_path(User.last)
+  end
+
+  test "should not create user with invalid attributes" do
+    assert_no_difference("User.count") do
+      post users_path, params: {
+        user: {
+          username: "",
+          email: "invalid",
+          password: "",
+          password_confirmation: ""
+        }
+      }
+    end
+    assert_response :unprocessable_entity
+  end
 end
